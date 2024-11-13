ccc9bf0e0e4a331cd83f3c4ca8c64d884d61ddfc
Gabriel <gabrielcorsoo282@gmail.com>
2024-11-13 19:58:29 -0300

    insertion

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿﻿﻿using System.Reflection.Metadata;
+using Insercao;
+
+string[] names = new string[3]{"Gabriel", "Corso", "Andrade"};
+
+Insercao.InsertionSort.Sort(names);
+
+foreach(string name in names)
+    System.Console.WriteLine(name);
